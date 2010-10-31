5d6f6bda1338923a26412f5601f754c84336afe4
James Rowe <jnrowe@gmail.com>
2010-10-31 20:49:23 +0000

    Rename bug argument to bugs, as it can be multiple.

--- gh-bugs.py
+++ gh-bugs.py
@@ -115,7 +115,7 @@ def process_command_line():
     show_parser = subparsers.add_parser("show", help="Displaying bugs")
     show_parser.add_argument("-f", "--full", action="store_true",
                              help="show bug including comments")
-    show_parser.add_argument("bug", nargs="+",
+    show_parser.add_argument("bugs", nargs="+",
                              help="bug number(s) to operate on")
     show_parser.set_defaults(func=show_bugs)
 
@@ -127,13 +127,13 @@ def process_command_line():
     comment_parser = subparsers.add_parser("comment",
                                            help="Commenting on bugs")
     comment_parser.add_argument("message", help="comment text")
-    comment_parser.add_argument("bug", nargs="+",
+    comment_parser.add_argument("bugs", nargs="+",
                                 help="bug number(s) to operate on")
     comment_parser.set_defaults(func=comment_bugs)
 
     close_parser = subparsers.add_parser("close", help="Closing bugs")
     close_parser.add_argument("message", help="comment text")
-    close_parser.add_argument("bug", nargs="+",
+    close_parser.add_argument("bugs", nargs="+",
                               help="bug number(s) to operate on")
     close_parser.set_defaults(func=close_bugs)
 
@@ -142,7 +142,7 @@ def process_command_line():
                               metavar="label")
     label_parser.add_argument("-r", "--remove", help="remove label from issue",
                               metavar="label")
-    label_parser.add_argument("bug", nargs="+",
+    label_parser.add_argument("bugs", nargs="+",
                               help="bug number(s) to operate on")
     label_parser.set_defaults(func=label_bugs)
 
