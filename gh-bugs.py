fcbffb5041c4c503944ac28aa06a62bea29df208
James Rowe <jnrowe@gmail.com>
2011-02-16 12:26:32 +0000

    Updated copyright years.

--- gh-bugs.py
+++ gh-bugs.py
@@ -1,6 +1,6 @@
 #! /usr/bin/python -tt
 """gh-bugs - Simple client for GitHub issues"""
-# Copyright (C) 2010  James Rowe <jnrowe@gmail.com>
+# Copyright (C) 2010-2011  James Rowe <jnrowe@gmail.com>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -19,7 +19,7 @@
 __version__ = "0.1.0"
 __date__ = "2010-10-31"
 __author__ = "James Rowe <jnrowe@gmail.com>"
-__copyright__ = "Copyright (C) 2010 James Rowe <jnrowe@gmail.com>"
+__copyright__ = "Copyright (C) 2010-2011  James Rowe <jnrowe@gmail.com>"
 __license__ = "GNU General Public License Version 3"
 __credits__ = ""
 __history__ = "See git repository"
