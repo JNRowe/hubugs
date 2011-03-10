509761af56b353bf93ec3e61c51aa44302d42c0a
James Rowe <jnrowe@gmail.com>
2011-03-10 16:21:57 +0000

    Fixed formatting of bug close times in builtin template.

--- gh_bugs/_version.py
+++ gh_bugs/_version.py
@@ -1,5 +1,5 @@
-# This is gh_bugs version 0.5.0 (2011-03-10)
-dotted = "0.5.0"
-libtool = "5:20"
-hex = 0x000500
+# This is gh_bugs version 0.5.1 (2011-03-10)
+dotted = "0.5.1"
+libtool = "5:21"
+hex = 0x000501
 date = "2011-03-10"
