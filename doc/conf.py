134e0ad076f182caafd082f1893f3a32825fcffc
James Rowe <jnrowe@gmail.com>
2011-06-15 05:49:44 +0100

    Ignore emacs temp files in Sphinx builds.

--- doc/conf.py
+++ doc/conf.py
@@ -67,7 +67,7 @@ release = _version.dotted
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['.build']
+exclude_patterns = ['.build', '.#*']
 
 # The reST default role (used for this markup: `text`) to use for all documents.
 #default_role = None
