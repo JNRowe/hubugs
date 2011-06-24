fdd5a98601bd9f32c62db9c32fffc9877152ed6a
James Rowe <jnrowe@gmail.com>
2011-06-24 14:19:13 +0100

    Updated distribute metadata.

--- hubugs/_version.py
+++ hubugs/_version.py
@@ -1,9 +1,9 @@
-# This is hubugs version 0.9.0 (2011-06-24)
+# This is hubugs version 0.9.1 (2011-06-24)
 # pylint: disable=C0103, C0111, C0121, W0622
 
-dotted = "0.9.0"
-libtool = "9:20"
-hex = 0x000900
+dotted = "0.9.1"
+libtool = "9:21"
+hex = 0x000901
 date = "2011-06-24"
-tuple = (0, 9, 0)
-web = "hubugs/0.9.0"
+tuple = (0, 9, 1)
+web = "hubugs/0.9.1"
