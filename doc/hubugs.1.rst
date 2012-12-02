ebbd0ce04397021f01e2024fed07f9dd1fd61654
James Rowe <jnrowe@gmail.com>
2012-12-02 20:32:55 +0000

    Follow GNU standard for copyright declarations.

--- doc/hubugs.1.rst
+++ doc/hubugs.1.rst
@@ -198,7 +198,7 @@ Issue tracker: https://github.com/JNRowe/hubugs/issues/
 COPYING
 -------
 
-Copyright © 2010-2012  James Rowe.
+Copyright © 2010, 2011, 2012  James Rowe.
 
 This program is free software: you can redistribute it and/or modify it
 under the terms of the GNU General Public License as published by the
--- hubugs.py
+++ hubugs.py
@@ -1,6 +1,7 @@
 #! /usr/bin/python -tt
+# coding=utf-8
 """hubugs - Simple client for GitHub issues"""
-# Copyright (C) 2010-2012  James Rowe <jnrowe@gmail.com>
+# Copyright © 2011, 2012  James Rowe <jnrowe@gmail.com>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
