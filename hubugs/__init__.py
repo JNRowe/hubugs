JNRowe/hubugs

commit 671b031769de79453e6c58f5d2a1d16cceb011a8

Use GitHub provided error message on HTTP client failure.
For example, we get "Bad credentials" instead of the old 401 for a broken token.
Closes #36.

hubugs/__init__.py
@@ -429,14 +429,17 @@ def main():
     parser.add_commands(COMMANDS)
     try:
         parser.dispatch(pre_call=utils.setup_environment)
-    except (utils.RepoError, EnvironmentError, ValueError) as error:
-        print utils.fail(error.message)
+    except utils.HttpClientError as error:
+        print utils.fail(error.content['message'])
         return errno.EINVAL
     except httplib2.ServerNotFoundError:
         print utils.fail(_("Project lookup failed.  Network or GitHub down?"))
         return errno.ENXIO
     except TypeException:
         print utils.fail(_("API modelling failed.  Please report this!"))
+    except (utils.RepoError, EnvironmentError, ValueError) as error:
+        print utils.fail(error.content['message'])
+        return errno.EINVAL
 
 if __name__ == '__main__':
     sys.exit(main())

commit 832833f187469cafab619738d80c76609e7f49ed

Fix base exception handling.
This fixes a bug introduced way back in
671b031769de79453e6c58f5d2a1d16cceb011a8, ick.

hubugs/__init__.py
@@ -475,6 +475,9 @@ def main():
     except TypeException:
         print(utils.fail(_("API modelling failed.  Please report this!")))
         return errno.EBADMSG
-    except (utils.RepoError, EnvironmentError, ValueError) as error:
+    except (utils.RepoError) as error:
         print(utils.fail(error.content['message']))
         return errno.EINVAL
+    except (EnvironmentError, ValueError) as error:
+         print(utils.fail(error.message))
+         return errno.EINVAL