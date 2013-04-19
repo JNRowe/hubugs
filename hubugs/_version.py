54164bdbae941835f8e5c1df08e11f92b869fdc3
James Rowe <jnrowe@gmail.com>
2013-04-19 01:23:43 +0100

    0.17.2 released

--- hubugs/_version.py
+++ hubugs/_version.py
@@ -1,9 +1,9 @@
-# This is hubugs version 0.17.1 (2013-02-02)
+# This is hubugs version 0.17.2 (2013-04-19)
 # pylint: disable=C0103, C0111, C0121, W0622
 
-dotted = '0.17.1'
-libtool = '17:21'
-hex = 0x001101
-date = '2013-02-02'
-tuple = (0, 17, 1)
-web = 'hubugs/0.17.1'
+dotted = '0.17.2'
+libtool = '17:22'
+hex = 0x001102
+date = '2013-04-19'
+tuple = (0, 17, 2)
+web = 'hubugs/0.17.2'
